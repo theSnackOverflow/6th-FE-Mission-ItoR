adc9421f212d8f303e02760deb3627300cd08f80
2ssac <ivory.ma9ic@gmail.com>
2025-10-14 02:52:04 +0900

    [#18]feat:Menu 컴포넌트 구현

--- my_blog/src/components/Menu.tsx
+++ my_blog/src/components/Menu.tsx
@@ -1,5 +1,15 @@
+import PolygonSVG from '@/assets/icons/polygon_1.svg?react';
+import DeleteSVG from '@/assets/icons/delete_forever.svg?react';
+
 const Menu = () => {
-  return <section className="w-[72px] h-[48px] dro-shadow-xl"></section>;
+  return (
+    <section className="flex flex-col justify-center items-center">
+      <div className="w-[72px] h-[48px] drop-shadow-xl bg-white rounded-sm flex justify-center items-center">
+        <DeleteSVG />
+      </div>
+      <PolygonSVG className="text-white" />
+    </section>
+  );
 };
 
 export default Menu;
