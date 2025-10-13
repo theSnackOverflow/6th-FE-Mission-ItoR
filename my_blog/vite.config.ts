346ce151badb3224fa2b2511037d1ccafa96943b
2ssac <ivory.ma9ic@gmail.com>
2025-10-14 02:01:30 +0900

    [#18]set:tailwindcss 설정

--- my_blog/vite.config.ts
+++ my_blog/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 import svgr from 'vite-plugin-svgr';
 
 export default defineConfig({
-  plugins: [react(), svgr()],
+  plugins: [react(), svgr(), tailwindcss()],
   resolve: {
     alias: {
       '@': '/src',
