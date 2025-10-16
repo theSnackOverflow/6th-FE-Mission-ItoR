58cc96270c02b7fffbd0cc7e6d386fb5b9a47ac0
2ssac <ivory.ma9ic@gmail.com>
2025-10-16 18:38:50 +0900

    [#28]wip:mainpage

--- my_blog/src/pages/main/MainPage.tsx
+++ my_blog/src/pages/main/MainPage.tsx
@@ -1,10 +1,62 @@
+import Blank from '../../components/Blank';
 import Header from '../../components/Header';
 
 const MainPage = () => {
   return (
     <>
       <Header />
-      <main></main>;
+      <main className="w-full h-screen flex flex-col justify-center items-center border bg-gray-50">
+        <section className="w-[688px] border">
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+          <Blank />
+        </section>
+      </main>
     </>
   );
 };
