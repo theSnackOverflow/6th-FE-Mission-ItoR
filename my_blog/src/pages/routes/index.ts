0cea1e63ccfb2f5ee1d0a0f732a62c4e114d2013
2ssac <ivory.ma9ic@gmail.com>
2025-10-16 17:45:21 +0900

    [#18]feat:/pages 디렉토리 구성

---
+++ my_blog/src/pages/routes/index.ts
@@ -0,0 +1,16 @@
+// import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// import MainPage from '@/pages/main/MainPage';
+// import MyPage from '@/pages/mypage/MyPage';
+// import SignUpPage from '@/pages/auth/SignUpPage';
+
+// const AppRouter = () => (
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<MainPage />} />
+//       <Route path="/mypage" element={<MyPage />} />
+//       <Route path="/signup" element={<SignUpPage />} />
+//     </Routes>
+//   </BrowserRouter>
+// );
+
+// export default AppRouter;
