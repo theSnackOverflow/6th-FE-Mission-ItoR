b6a6e5cd9b031a0e78f72ef39b74ad44d1e890fe
2ssac <ivory.ma9ic@gmail.com>
2025-10-18 19:51:31 +0900

    [#28]feat:Header others 버튼 동작  구현

--- my_blog/src/components/DropdownMenu.tsx
+++ my_blog/src/components/DropdownMenu.tsx
@@ -1,10 +1,19 @@
 import PolygonSVG from '@/assets/icons/polygon_1.svg?react';
-
 import DropdownMenuMaterial from './DropdownMenuMaterial';
+import clsx from 'clsx';
 
-const DropdownMenu = () => {
+interface dropdownMenuProps {
+  className?: string;
+}
+
+const DropdownMenu = ({ className }: dropdownMenuProps) => {
   return (
-    <section className="relative">
+    <section
+      className={clsx(
+        'absolute shadow-[0_2px_8px_0_rgba(0,0,0,0.10)] ',
+        className,
+      )}
+    >
       <div className="absolute -top-2 right-0 flex justify-end mr-1.5">
         <PolygonSVG className="text-white rotate-180" />
       </div>
--- my_blog/src/components/Header.tsx
+++ my_blog/src/components/Header.tsx
@@ -1,10 +1,13 @@
+import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 import MenuIcon from '../assets/icons/reorder.svg?react';
 import PencleIcon from '../assets/icons/create.svg?react';
 import CommentIcon from '../assets/icons/chat.svg?react';
 import OthersIcon from '../assets/icons/more_vert.svg?react';
 import AddPhotoIcon from '../assets/icons/add_photo_alternate.svg?react';
 import AddFileIcon from '../assets/icons/folder_open.svg?react';
+import DropdownMenu from './DropdownMenu';
 
 type headerType = 'main' | 'detail' | 'write' | 'file';
 
@@ -29,12 +32,32 @@ const Header = ({
 }: HeaderProps) => {
   const navigate = useNavigate();
 
+  const [showDropdown, setShowDropdown] = useState(false);
+
+  useEffect(() => {
+    const handleClickOutside = (e: MouseEvent) => {
+      const dropdown = document.querySelector('.dropdown-menu');
+      const trigger = document.querySelector('.others-icon');
+      if (
+        dropdown &&
+        !dropdown.contains(e.target as Node) &&
+        trigger &&
+        !trigger.contains(e.target as Node)
+      ) {
+        setShowDropdown(false);
+      }
+    };
+
+    window.addEventListener('click', handleClickOutside);
+    return () => window.removeEventListener('click', handleClickOutside);
+  }, []);
+
   return type !== 'file' ? (
     <nav
       className="fixed top-0 left-0 right-0 px-3 py-4 bg-white opacity-90 border-b border-gray-96 backdrop-blur-[2px] min-w-mobile"
       style={{ zIndex: 50, top: offsetTop }}
     >
-      <div className="flex justify-between items-center">
+      <div className="relative flex justify-between items-center">
         <div className="flex justify-center items-center ">
           <MenuIcon
             className="w-10 h-10 p-2 cursor-pointer"
@@ -66,8 +89,17 @@ const Header = ({
             onClick={() => {}}
           >
             <CommentIcon className="w-10 h-10 p-2" onClick={() => {}} />
-            <OthersIcon className="w-10 h-10 p-2" onClick={() => {}} />
+            <OthersIcon
+              className="others-icon w-10 h-10 p-2"
+              onClick={() => {
+                setShowDropdown((prev) => !prev);
+              }}
+            />
           </button>
+        )}
+        {/* 드롭다운 */}
+        {showDropdown && (
+          <DropdownMenu className={'dropdown-menu top-10 right-1.5'} />
         )}
         {type === 'write' && (
           <div className="w-fit h-fit flex items-center">
