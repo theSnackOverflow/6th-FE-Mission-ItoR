35121578d1c3cf3c096e4b12e412f96a8ee582a4
2ssac <ivory.ma9ic@gmail.com>
2025-10-19 20:37:21 +0900

    [#28]refactor:Mypage 렌더링을 위한 PostList 컴포넌트 리팩토링

--- my_blog/src/pages/main/components/PostList.tsx
+++ my_blog/src/pages/main/components/PostList.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { v4 as uuidv4 } from 'uuid';
 import { useState } from 'react';
 
@@ -5,24 +6,23 @@ import Blank from '../../../components/Blank';
 import Pagination from '../../../components/Pagination/Pagination';
 import { PostItem } from './PostItem';
 
-import { mockData } from '../../../const/mockData';
-
 const ITEMS_PER_PAGE = 10;
 
-const PostList = () => {
+//! 추후 api 연결 시 posts 관련 type 정의
+const PostList = ({ posts }: any) => {
   const [currentPage, setCurrentPage] = useState(1);
 
-  const totalPages = Math.ceil(mockData.length / ITEMS_PER_PAGE);
+  const totalPages = Math.ceil(posts.length / ITEMS_PER_PAGE);
 
   const startIndex = (currentPage - 1) * ITEMS_PER_PAGE;
   const endIndex = startIndex + ITEMS_PER_PAGE;
-  const currentPosts = mockData.slice(startIndex, endIndex);
+  const currentPosts = posts.slice(startIndex, endIndex);
 
   return (
     <section className="w-full max-w-[688px] min-w-mobile h-fit flex flex-col justify-between">
       <div>
         <Blank />
-        {currentPosts.map((post) => (
+        {currentPosts.map((post: any) => (
           <PostItem
             key={`${post.postId} - ${uuidv4()}`}
             {...post}
@@ -31,7 +31,7 @@ const PostList = () => {
         ))}
         <Blank variant="20" />
       </div>
-      <div className="">
+      <div>
         <Pagination
           totalPages={totalPages}
           initialPage={1}
