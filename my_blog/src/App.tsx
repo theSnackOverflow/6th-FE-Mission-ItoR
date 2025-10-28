b3510044e49c336b74b99f032e7652ca5cd935d7
2ssac <ivory.ma9ic@gmail.com>
2025-10-28 23:26:53 +0900

    [#36]api:/auth/reissue api 연동

--- my_blog/src/App.tsx
+++ my_blog/src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import axiosInstance from './api/axiosInstance';
 import {
   TestForBlank,
   TestForButton,
@@ -31,6 +33,31 @@ import OAuthSignUp from './pages/auth/OAthSignUp';
 import KakaoRedirect from './pages/auth/KakaoRedirect';
 
 function App() {
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+    const refreshToken = localStorage.getItem('refreshToken');
+
+    if (token) {
+      axiosInstance.defaults.headers.common.Authorization = `Bearer ${token}`;
+    } else if (refreshToken) {
+      const refreshAccessToken = async () => {
+        try {
+          const res = await axiosInstance.post('/auth/reissue', {
+            refreshToken,
+          });
+          const newToken = res.data.data.accessToken;
+          localStorage.setItem('accessToken', newToken);
+          axiosInstance.defaults.headers.common.Authorization = `Bearer ${newToken}`;
+        } catch (err) {
+          console.error('토큰 갱신 실패:', err);
+          localStorage.removeItem('accessToken');
+          localStorage.removeItem('refreshToken');
+          window.location.href = '/';
+        }
+      };
+      refreshAccessToken();
+    }
+  }, []);
   return (
     <BrowserRouter>
       <Routes>
--- my_blog/src/components/Button/LoginButton.tsx
+++ my_blog/src/components/Button/LoginButton.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 
 import KakaoIcon from '../../assets/icons/kakao.svg?react';
 
-type LoginButtonVariant = 'EMAILOGIN' | 'KAKOLOGIN';
+type LoginButtonVariant = 'EMAILOGIN' | 'KAKAOLOGIN';
 
 interface loginButtonProps {
   type: LoginButtonVariant;
@@ -12,7 +12,7 @@ interface loginButtonProps {
 
 const varaintMap: Record<LoginButtonVariant, string> = {
   EMAILOGIN: 'text-white text-sm font-normal  bg-point',
-  KAKOLOGIN: 'flex justify-center items-center gap-2 text-[15px] bg-kakao-bg',
+  KAKAOLOGIN: 'flex justify-center items-center gap-2 text-[15px] bg-kakao-bg',
 };
 
 const LoginButton = ({ type, text, onClick }: loginButtonProps) => {
@@ -21,7 +21,7 @@ const LoginButton = ({ type, text, onClick }: loginButtonProps) => {
       className={clsx('w-full h-11 my-1 px-3.5 rounded-md', varaintMap[type])}
       onClick={onClick}
     >
-      {type === 'KAKOLOGIN' && <KakaoIcon className="w-4" />}
+      {type === 'KAKAOLOGIN' && <KakaoIcon className="w-4" />}
       {text}
     </button>
   );
