f2ab2f689d6e9e52159a1b580a8da59e4096314a
2ssac <ivory.ma9ic@gmail.com>
2025-11-04 23:15:47 +0900

    [#49]api:DELETE /comments/{postId}-댓글 삭제- api 구현

--- my_blog/src/api/commentAPI.ts
+++ my_blog/src/api/commentAPI.ts
@@ -1,6 +1,17 @@
 import axiosInstance from './axiosInstance';
+import { axiosPrivateInstance } from '@/api/axiosInstance';
 
 export const createComment = async (postId: string, content: string) => {
   const response = await axiosInstance.post(`/comments/${postId}`, { content });
   return response.data;
+};
+
+export const deleteComment = async (commentId: number) => {
+  try {
+    const res = await axiosPrivateInstance.delete(`/comments/${commentId}`);
+    return res.data;
+  } catch (error) {
+    console.error('Failed to delete comment:', error);
+    throw error;
+  }
 };
