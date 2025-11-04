05a0c973436153e5e7f97a159175f30e8f643888
2ssac <ivory.ma9ic@gmail.com>
2025-11-04 22:00:20 +0900

    [#49]api:DELELTE /posts-게시물 삭제- api 구현

--- my_blog/src/api/postAPI.ts
+++ my_blog/src/api/postAPI.ts
@@ -1,6 +1,26 @@
 import axiosInstance from './axiosInstance';
 
+// 토큰 없이 게시물 조회
 export const getPostById = async (postId: number) => {
   const response = await axiosInstance.get('/posts', { params: { postId } });
   return response.data.data;
+};
+
+// 게시물 생성
+export const createPost = async (postData: {
+  title: string;
+  contents: {
+    contentOrder: number;
+    content: string;
+    contentType: 'TEXT' | 'IMAGE';
+  }[];
+}) => {
+  const response = await axiosInstance.post('/posts', postData);
+  return response.data;
+};
+
+// 게시물 삭제
+export const deletePost = async (postId: string) => {
+  const response = await axiosInstance.delete('/posts', { params: { postId } });
+  return response.data;
 };
