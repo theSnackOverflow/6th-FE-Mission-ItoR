3ac2414ed236946d65faba32601072fa8c57b676
2ssac <ivory.ma9ic@gmail.com>
2025-11-11 22:46:40 +0900

    [#63]api:PATCH /users/nickname api 구현

--- my_blog/src/api/userAPI.ts
+++ my_blog/src/api/userAPI.ts
@@ -1,5 +1,11 @@
 import { axiosPrivateInstance } from './axiosInstance';
 
+// 내 정보 조회
+export const getMyInfo = async () => {
+  const response = await axiosPrivateInstance.get('/users/me');
+  return response.data.data;
+};
+
 // 유저 정보 수정
 export const updateUser = async (userData: {
   email?: string;
@@ -13,8 +19,10 @@ export const updateUser = async (userData: {
   return response.data;
 };
 
-// 내 정보 조회
-export const getMyInfo = async () => {
-  const response = await axiosPrivateInstance.get('/users/me');
-  return response.data.data;
+// 닉네임 수정 (PATCH /users/nickname)
+export const updateNickname = async (nickname: string) => {
+  const response = await axiosPrivateInstance.patch('/users/nickname', {
+    nickname,
+  });
+  return response.data;
 };
