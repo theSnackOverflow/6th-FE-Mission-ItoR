666efea0f62ff3ef84607767aee21e966368e8b1
2ssac <ivory.ma9ic@gmail.com>
2025-11-23 12:12:17 +0900

    fix:직접 DOM을 제어하지 않고 PostEdit.tsx에서 ref를 사용하도록 수정

--- my_blog/src/components/Menu.tsx
+++ my_blog/src/components/Menu.tsx
@@ -1,3 +1,4 @@
+import { forwardRef } from 'react';
 import PolygonSVG from '@/assets/icons/polygon_1.svg?react';
 import DeleteSVG from '@/assets/icons/delete_forever.svg?react';
 
@@ -8,28 +9,33 @@ interface MenuProps {
   onClose: () => void;
 }
 
-const Menu = ({ top, left, onDelete, onClose }: MenuProps) => {
-  return (
-    <section
-      className="absolute top-0 z-50 flex flex-col justify-center items-center"
-      style={{ top: top, left: left }}
-    >
-      <div
-        className="w-[72px] h-12 px-4 py-1 
-                  flex justify-center items-center 
-                bg-white rounded-sm shadow-lg"
+const Menu = forwardRef<HTMLElement, MenuProps>(
+  ({ top, left, onDelete, onClose }, ref) => {
+    return (
+      <section
+        ref={ref}
+        className="absolute top-0 z-50 flex flex-col justify-center items-center"
+        style={{ top: top, left: left }}
       >
-        <DeleteSVG
-          className="hover:text-negative cursor-pointer"
-          onClick={() => {
-            onDelete();
-            onClose();
-          }}
-        />
-      </div>
-      <PolygonSVG className="text-white" />
-    </section>
-  );
-};
+        <div
+          className="w-[72px] h-12 px-4 py-1
+                  flex justify-center items-center
+                bg-white rounded-sm shadow-lg"
+        >
+          <DeleteSVG
+            className="hover:text-negative cursor-pointer"
+            onClick={() => {
+              onDelete();
+              onClose();
+            }}
+          />
+        </div>
+        <PolygonSVG className="text-white" />
+      </section>
+    );
+  },
+);
+
+Menu.displayName = 'Menu';
 
 export default Menu;
