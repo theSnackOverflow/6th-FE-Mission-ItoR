2b5edf04b438268d31fa4e5e8d1b1155c6311815
2ssac <ivory.ma9ic@gmail.com>
2025-11-23 12:14:42 +0900

    fix:불필요한 프록시 설정 제거

--- my_blog/vite.config.ts
+++ my_blog/vite.config.ts
@@ -13,12 +13,5 @@ export default defineConfig({
   },
   server: {
     port: 3000,
-    proxy: {
-      '/auth': {
-        target: 'https://blog.leets.land',
-        changeOrigin: true,
-        secure: true,
-      },
-    },
   },
 });
