cce74b19c4429e704afc0d080a01dd23ab691595
2ssac <ivory.ma9ic@gmail.com>
2025-11-23 12:37:47 +0900

    fix:as 타입 단언을 제거 -> Axios 제네릭

--- my_blog/src/api/authAPI.ts
+++ my_blog/src/api/authAPI.ts
@@ -11,8 +11,8 @@ import type {
 
 export const signUp = async (data: SignUpRequest): Promise<SignUpResponse> => {
   try {
-    const res = await axiosInstance.post('/auth/register', data);
-    return res.data as SignUpResponse;
+    const res = await axiosInstance.post<SignUpResponse>('/auth/register', data);
+    return res.data;
   } catch (error) {
     console.error('회원가입 요청 실패', error);
     throw error;
@@ -33,8 +33,11 @@ export const signUpOAuth = async (
   payload: SignUpOAuthRequest,
 ): Promise<SignUpOAuthResponse> => {
   try {
-    const res = await axiosInstance.post('/auth/register-oauth', payload);
-    return res.data as SignUpOAuthResponse;
+    const res = await axiosInstance.post<SignUpOAuthResponse>(
+      '/auth/register-oauth',
+      payload,
+    );
+    return res.data;
   } catch (error) {
     console.error('OAuth 회원가입 요청 실패', error);
     throw error;
